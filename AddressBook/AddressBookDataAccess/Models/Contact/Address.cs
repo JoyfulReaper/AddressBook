e6c7e6b7ff54cc8eb772950c3ec39a436c282139
IsaacVallejo <49381080+IsaacVallejo@users.noreply.github.com>
2020-10-21 19:46:35 -0700

    updated address.cs

--- AddressBook/AddressBookDataAccess/Models/Contact/Address.cs
+++ AddressBook/AddressBookDataAccess/Models/Contact/Address.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 
 namespace AddresMVC.Models
 {
+    /*
+    Add bool checker for address. 'IsMailAddress'
+    this will check if whether the address is also the person's mail address
+    */
+    
     public class Address
     {
         public int Id { get; set; }
-        public string MailAddress { get; set; }
+        public int PersonId { get; set; }
+        public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
