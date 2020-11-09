70455704ee0cbdf4809f032bec99a04743851ad4
Ryan Flemming <rmf919@live.com>
2020-11-09 14:48:24 +1100

    Add create person view model

---
+++ AddressBook/AddressBookMVC/Models/People/PersonSubmitViewModel.cs
@@ -0,0 +1,28 @@
+﻿using AddressBookDataAccess.Models.Contact;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Models.People
+{
+    public class PersonSubmitViewModel
+    {
+        public int PersonId { get; set; }
+        public int EmailId { get; set; }
+        public int AddressId { get; set; }
+        public int PhoneNumberId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public List<Email> EmailAddresses { get; set; }
+        [Required]
+        public List<Address> Addresses { get; set; }
+        [Required]
+        public List<PhoneNum> PhoneNumbers { get; set; }
+    }
+}
