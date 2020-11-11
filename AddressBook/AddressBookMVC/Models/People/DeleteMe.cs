689ce2f6614162dc6bafe2912d60c631d989b01e
Ryan Flemming <rmf919@live.com>
2020-11-12 10:58:30 +1100

    Move VMs to models folder (#22)
    * Mirrored folder structure in Models in DataAccess, in ViewModels folder
    
    * Relocate VM subfolders to Models in MVC project
    
    * Made 'Models' mirror the data access tree and ViewModels separate from models
    
    * Fixed merge conflict by removing unneeded reference in csproj

---
+++ AddressBook/AddressBookMVC/Models/People/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookMVC.Models.People
+{
+    public class DeleteMe
+    {
+    }
+}
