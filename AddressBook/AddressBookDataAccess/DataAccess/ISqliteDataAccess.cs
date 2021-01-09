2339891d9d1c24a28adcfb873323f329a9f46055
Sam Lee <sflee36@yahoo.com.au>
2021-01-09 17:47:22 +0800

    Created generic query multiple method
    Uses reflection to determine which properties are generic
    
    Generic types are used to query result set
    
    Data is set through reflection

--- AddressBook/AddressBookDataAccess/DataAccess/ISqliteDataAccess.cs
+++ AddressBook/AddressBookDataAccess/DataAccess/ISqliteDataAccess.cs
@@ -6,7 +6,8 @@ namespace AddressBookDataAccess.DataAccess
     {
         List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString);
         void SaveData<T>(string sqlStatement, T parameters, string connectionString);
-        
+        T LoadResultSets<T, U>(string sqlStatements, U parameters, string connectionString);
+
         void RollbackTransaction();
         List<T> LoadDataInTransaction<T, U>(string sqlStatement, U parameters);
         void SaveDataInTransaction<T>(string sqlStatement, T parameters);
